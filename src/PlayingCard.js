6af829174800977f64b1c02ad2175b071b23e3f6
ryan <ryanjgecelter@gmail.com>
2022-08-12 08:43:49 +0300

    refactor PlayingCard with useToggleState

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
 import "./PlayingCard.css"
+import useToggleState from "./hooks/useToggleState";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, flipCard] = useToggleState();
+
   return (
     <img
       src={isFacingUp ? front : back}
